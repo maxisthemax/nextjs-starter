9c5a9e1d42a97a4df1b039154436cf9f34c19685
maxgraaab <51647099+maxgraaab@users.noreply.github.com>
2021-09-24 15:06:54 +0800

    init 5

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,5 @@
 import Container from "@mui/material/Container";
-import { ColorButtonText } from "components/Buttons";
 
 export default function Index() {
-  return (
-    <Container maxWidth="sm">
-      <ColorButtonText
-        variant="contained"
-        color="primary"
-        innerText="Hellow World"
-        onClick={() => alert("123")}
-      />
-    </Container>
-  );
+  return <Container maxWidth="sm"></Container>;
 }
