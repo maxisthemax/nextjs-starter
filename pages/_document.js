df2f89cf2ba946cc134bffe496cf531542b5b8e4
maxgraaab <51647099+maxgraaab@users.noreply.github.com>
2021-09-24 12:08:52 +0800

    init 2

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
 import theme from "theme";
-import createEmotionCache from "createEmotionCache";
+import createEmotionCache from "utils/createEmotionCache";
 
 export default class MyDocument extends Document {
   render() {
