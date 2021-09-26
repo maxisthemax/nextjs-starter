a50b237efe921dc07f9f0dfe8f119efb2055ba4a
maxgraaab <51647099+maxgraaab@users.noreply.github.com>
2021-09-26 15:46:49 +0800

    add component template

---
+++ .custom-template/component.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+//*lodash
+
+//*components
+
+//*material-ui
+import Box from "@mui/material/Box";
+import { makeStyles } from "@mui/styles";
+
+//*assets
+
+//*redux
+
+//*utils
+
+//*helpers
+
+//*style
+
+//*custom components
+
+function ${1}() {
+  //*define
+
+  //*states
+
+  //*const
+
+  //*let
+
+  //*ref
+
+  //*useEffect
+
+  //*functions
+
+  return <div></div>;
+}
+
+export default ${1};
